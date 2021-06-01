cf9e1ca41fb6de18d575ec0ba8e3fa3c10f75e09
hpnrep6 <hpnrep6@gmail.com>
2021-06-01 14:47:33 -0400

    new alg

--- index.js
+++ index.js
@@ -81,63 +81,25 @@ export class Main extends Scene {
 
         this.setBackgroundColour(125 / 255 + off, 73 / 255 + off, 21 / 255 + off, 1);
 
+
+        let x = getMouseX()
+        let y = getMouseY()
+
+        let r = 50;
+
+        for(let i = x - r; i < x + r; i += Grid.size) {
+            for(let j = y - r; j < y + r; j += Grid.size) {
+                this.grid.setValueAt(i, j, 0);
+            }
+        }
+
+        this.grid.updateGraphics()
+
         if(Key.isKeyDown('w') ) {
             return
         }
 
         this.grid.update();
-    //     if(this.con > -1) {
-    //     let n = [];
-
-    //     for(let i = 0; i < this.sizew; i++) {
-    //         n.push([])
-    //         for(let j = 0; j < this.sizee; j++) {
-    //             n[i][j] = this.a[i][j]
-    //         }
-    //     }
-
-
-    //     for(let i = 1; i < this.sizew-1; i++) {
-    //         for(let j = 1; j < this.sizee-1; j++) {
-    //             let al = 0;
-    //             for(let k = i - 1; k < i + 2; k++) {
-    //                 for(let l = j - 1; l < j + 2; l++) {
-    //                     if(this.a[k][l] == 0) {
-    //                         if(k == i && l == j)
-    //                             continue
-    //                         al++;
-    //                     }
-    //                 }
-    //             }
-
-    //             if(this.a[i][j] == 0) {
-    //                 if(al < 2) {
-    //                     n[i][j] = 1
-    //                 } else if(al > 4) {
-    //                     n[i][j] = 1
-    //                 }
-    //             } else {
-    //                 if(al == 3 || al == 4) {
-    //                     n[i][j] = 0
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     this.a = n;
-        
-    //     for(let i = 0; i < this.sizew; i++) {
-
-    //         for(let j = 0; j < this.sizee; j++) {
-    //             if(this.a[i][j] == 0)
-    //                 this.arr[i][j].setSprite(0)
-    //             else
-    //                 this.arr[i][j].setSprite(1)
-    //         }
-    //     }
-    //     this.con = 0
-    // } else this.con++
-        
     }
 
     start() {
@@ -169,7 +131,7 @@ class Grid {
     static ALIVE_2 = 2;
     static ALIVE_3 = 3;
 
-    static size = 5;
+    static size = 8;
     static width = canvas.width / Grid.size;
     static height = canvas.height / Grid.size;
    
@@ -198,44 +160,62 @@ class Grid {
             }
         }
 
-        let cur = this.buffer1;
-
-        cur[25][25] = Grid.ALIVE_1;
+        this.buffer1[25][25] = Grid.ALIVE_1;
 
         let x = 23, y = 25
-        cur[x+1][y+1] = Grid.ALIVE_1
-        cur[x+1][y+2] = Grid.ALIVE_1
-        cur[x+2][y+1] = Grid.ALIVE_1
-        cur[x+3][y+1] = Grid.ALIVE_1
-        cur[x+2][y+3] = Grid.ALIVE_1
+        this.buffer1[x+1][y+1] = Grid.ALIVE_1
+        this.buffer1[x+1][y+2] = Grid.ALIVE_1
+        this.buffer1[x+2][y+1] = Grid.ALIVE_1
+        this.buffer1[x+3][y+1] = Grid.ALIVE_1
+        this.buffer1[x+2][y+3] = Grid.ALIVE_1
 
 
          x = 55, y = 53
-        cur[x+1][y+1] = Grid.ALIVE_2
-        cur[x+1][y+2] = Grid.ALIVE_2
-        cur[x+2][y+1] = Grid.ALIVE_2
-        cur[x+3][y+1] = Grid.ALIVE_2
-        cur[x+2][y+3] = Grid.ALIVE_2
+        this.buffer1[x+1][y+1] = Grid.ALIVE_2
+        this.buffer1[x+1][y+2] = Grid.ALIVE_2
+        this.buffer1[x+2][y+1] = Grid.ALIVE_2
+        this.buffer1[x+3][y+1] = Grid.ALIVE_2
+        this.buffer1[x+2][y+3] = Grid.ALIVE_2
         x = 56, y = 55
-        cur[x+1][y+1] = Grid.ALIVE_2
-        cur[x+1][y+2] = Grid.ALIVE_2
-        cur[x+2][y+1] = Grid.ALIVE_2
-        cur[x+3][y+1] = Grid.ALIVE_2
-        cur[x+2][y+3] = Grid.ALIVE_2
+        this.buffer1[x+1][y+1] = Grid.ALIVE_2
+        this.buffer1[x+1][y+2] = Grid.ALIVE_2
+        this.buffer1[x+2][y+1] = Grid.ALIVE_2
+        this.buffer1[x+3][y+1] = Grid.ALIVE_2
+        this.buffer1[x+2][y+3] = Grid.ALIVE_2
 
         x =67, y = 25
-        cur[x+1][y+1] = Grid.ALIVE_3
-        cur[x+1][y+2] = Grid.ALIVE_3
-        cur[x+2][y+1] = Grid.ALIVE_3
-        cur[x+3][y+1] = Grid.ALIVE_3
-        cur[x+2][y+3] = Grid.ALIVE_3
+        this.buffer1[x+1][y+1] = Grid.ALIVE_3
+        this.buffer1[x+1][y+2] = Grid.ALIVE_3
+        this.buffer1[x+2][y+1] = Grid.ALIVE_3
+        this.buffer1[x+3][y+1] = Grid.ALIVE_3
+        this.buffer1[x+2][y+3] = Grid.ALIVE_3
         x =69, y = 22
-        cur[x+1][y+1] = Grid.ALIVE_3
-        cur[x+1][y+2] = Grid.ALIVE_3
-        cur[x+2][y+1] = Grid.ALIVE_3
-        cur[x+3][y+1] = Grid.ALIVE_3
-        cur[x+2][y+3] = Grid.ALIVE_3
+        this.buffer1[x+1][y+1] = Grid.ALIVE_3
+        this.buffer1[x+1][y+2] = Grid.ALIVE_3
+        this.buffer1[x+2][y+1] = Grid.ALIVE_3
+        this.buffer1[x+3][y+1] = Grid.ALIVE_3
+        this.buffer1[x+2][y+3] = Grid.ALIVE_3
 
+    }
+
+    setValueAt(x, y, v) {
+        x -= this.xLoc;
+        y -= this.yLoc;
+
+        x /= Grid.size;
+        y /= Grid.size;
+
+        x += .5;
+        y += .5;
+
+        x = Math.floor(x);
+        y = Math.floor(y);
+
+        if(x < 0 || x >= Grid.width) return;
+        if(y < 0 || y >= Grid.height) return;
+        
+        this.buffer1[x][y] = v;
+        this.buffer2[x][y] = v;
     }
 
     steps = 0;
@@ -243,19 +223,13 @@ class Grid {
     update() {
         this.steps++;
 
-        let cur, next;
+        let next;
 
-        if(this.usingBuffer1) {
-            cur = this.buffer1;
-            next = this.buffer2;
-        } else {
-            cur = this.buffer2;
-            next = this.buffer1;
-        }
 
         next = [];
+
         for(let i = 0; i < Grid.width; i++) {
-            next.push([])
+            next.push(new Int16Array(Grid.height))
             for(let j = 0; j < Grid.height; j++) {
                 next[i][j] = this.buffer1[i][j]
             }
@@ -277,19 +251,19 @@ class Grid {
 
         for(let i = 1; i < Grid.width -1; i++) {
             for(let j = 1; j < Grid.height -1; j++) {
-                switch(cur[i][j]) {
+                switch(this.buffer1[i][j]) {
                     case Grid.DEAD: {
                         let alive = [
-                            0,0,0,0
+                            0,0,0
                         ]
 
                         for(let k = i - 1; k < i + 2; k++) {
                             for(let l = j - 1; l < j + 2; l++) {
-                                if(cur[k][l] > Grid.DEAD) {
+                                if(this.buffer1[k][l] > Grid.DEAD) {
                                     
                                     if(k == i && l == j)
                                         continue
-                                    alive[cur[k][l]]++;
+                                    alive[this.buffer1[k][l]]++;
                                 }
                             }
                         }
@@ -314,14 +288,14 @@ class Grid {
                         let al = 0;
                         for(let k = i - 1; k < i + 2; k++) {
                             for(let l = j - 1; l < j + 2; l++) {
-                                if(cur[k][l] == Grid.ALIVE_3) {
+                                if(this.buffer1[k][l] == Grid.ALIVE_3) {
 
                                     let c = (Math.cos((k * i + l * j + 11)) * 131 + this.steps) % 6
 
                                     if(c > 4)
                                         next[k][l] = Grid.ALIVE_1
                                 }
-                                else if(cur[k][l] == Grid.ALIVE_1) {
+                                else if(this.buffer1[k][l] == Grid.ALIVE_1) {
                                     if(k == i && l == j)
                                         continue
                                     al++;
@@ -329,11 +303,11 @@ class Grid {
                             }
                         }
 
-                        if(al < 1) {
-                            next[i][j] = Grid.DEAD
-                        } else if(al > 4) {
-                            next[i][j] = Grid.DEAD
-                        }
+                        // if(al < 1) {
+                        //     next[i][j] = Grid.DEAD
+                        // } else if(al > 4) {
+                        //     next[i][j] = Grid.DEAD
+                        // }
 
                     } break;
 
@@ -343,14 +317,14 @@ class Grid {
                         let al = 0;
                         for(let k = i - 1; k < i + 2; k++) {
                             for(let l = j - 1; l < j + 2; l++) {
-                                if(cur[k][l] == Grid.ALIVE_1) {
+                                if(this.buffer1[k][l] == Grid.ALIVE_1) {
 
                                     let c = (Math.cos((k * i + l * j + 121) + this.steps) * 131) % 6
 
                                     if(c > 4)
                                         next[k][l] = Grid.ALIVE_2
                                 }
-                                else if(cur[k][l] == Grid.ALIVE_2) {
+                                else if(this.buffer1[k][l] == Grid.ALIVE_2) {
                                     if(k == i && l == j)
                                         continue
                                     al++;
@@ -371,14 +345,14 @@ class Grid {
                         let al = 0;
                         for(let k = i - 1; k < i + 2; k++) {
                             for(let l = j - 1; l < j + 2; l++) {
-                                if(cur[k][l] == Grid.ALIVE_2) {
+                                if(this.buffer1[k][l] == Grid.ALIVE_2) {
 
                                     let c = (Math.cos((k * i + l * j + 131)) * 531 + this.steps) % 6
 
                                     if(c > 3)
                                         next[k][l] = Grid.ALIVE_3
                                 }
-                                else if(cur[k][l] == Grid.ALIVE_3) {
+                                else if(this.buffer1[k][l] == Grid.ALIVE_3) {
                                     if(k == i && l == j)
                                         continue
                                     al++;
@@ -386,24 +360,25 @@ class Grid {
                             }
                         }
 
-                        if(al < 1) {
-                            next[i][j] = Grid.DEAD
-                        } else if(al > a_3_ind) {
-                            next[i][j] = Grid.DEAD
-                        }
+                        // if(al < 1) {
+                        //     next[i][j] = Grid.DEAD
+                        // } else if(al > a_3_ind) {
+                        //     next[i][j] = Grid.DEAD
+                        // }
                     } break;
                 }
             }
         }
-        
+    
+        this.buffer1 = next;
+    }
+
+    updateGraphics() {
         for(let i = 1; i < Grid.width - 1; i++) {
             for(let j = 1; j < Grid.height - 1; j++) {
-                this.tiles[i][j].setSprite(next[i][j])
-                //this.tiles[i][j].setSprite(Math.trunc(next[i][j]))
+                this.tiles[i][j].setSprite(this.buffer1[i][j])
             }
         }
-
-        this.buffer1 = next;
     }
 }
 
